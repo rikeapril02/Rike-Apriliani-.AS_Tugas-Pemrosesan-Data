4efac25ac2330f0ab5c06a212d6971c8f92b2a54
rikeapril02 <71260031+rikeapril02@users.noreply.github.com>
2020-09-28 07:40:33 -0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -1,1 +1,17 @@
+<?php
+$bilangan1 = $_GET['bilangan1'];
+$bilangan2 = $_GET['bilangan2'];
+$operasi = $_GET['operasi'];
+echo "Bilangan 1 yang dikirimkan adalah $bilangan1 <br>";
+echo "Bilangan 2 yang dikirimkan adalah $bilangan2 <br>";
 
+$bil1 = $_GET['bilangan1']; $bilangan2 = $_GET['bilangan2'];
+$operasi  = $_GET['operasi']; 
+echo '<output>'; if ($operasi == "tambah") 
+$hasil = $bilangan1 + $bilangan2; else if ($operasi == "kurang") 
+$hasil = $bilangan1 - $bilangan2; else if ($operasi == "kali") 
+$hasil = $bilangan1 * $bilangan2; else if ($operasi == "bagi")   
+$hasil = $bilangan1 / $bilangan2;      
+ echo "Hasil perhitungannya adalah : ". $hasil; echo '</output>';
+
+?>
